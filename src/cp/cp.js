b5bb539f27e2d7af5685298a20043a48274421f0
sarafashka <96177099+sarafashka@users.noreply.github.com>
2022-12-06 00:37:19 +0200

    feat: child_process done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,20 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file = path.join(__dirname, 'files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [`${file}`, ...args.split(' ') ]);
+    process.stdin.on('data', (data) => {
+      child.stdin.write(data);
+    })
+
+    child.stdout.on('data', (data) => {
+      console.log(data.toString());
+    })
 };
 
-spawnChildProcess();
+spawnChildProcess('hello bye');
